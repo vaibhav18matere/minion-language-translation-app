1993c2bc58033b20b5895b3d35f3281d321cbbbc
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2020-12-16 02:16:09 +0530

    minion pack import and input added

--- app.js
+++ app.js
@@ -2,13 +2,27 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output")
 
-// outputDiv.innerText ="Vaibhav Matere"
+var serverUrl = "	https://api.funtranslations.com/translate/minion.json"
 
+function getTranslationUrl(text) {
+    return serverUrl + "?" + "text" + text
+}
 
+function getErrorHandler(text) {
+    console.log("error occured ", error);
+    alert("something wrong with server : try after some time")
+}
 
 function clickHandler() {
-    outputDiv.innerText = "banananaa " + txtInput.value;
+    var inputText = txtInput.nodeValue;
 
+    fetch(getTranslationUrl(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translationText=json.contents.translated;
+            outputDiv.innerText=translatedText;
+        })
+        .catch(errorHandler)
 };
 
-btnTranslate.addEventListener("click", clickHandler)s
+btnTranslate.addEventListener("click", clickHandler) 
