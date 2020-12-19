eb877a0fc1b1d5ef9a35d0d3d62baedffb013b06
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2020-12-19 21:59:45 +0530

    erroe handling

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var outputDiv = document.querySelector("#output")
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationURL(input) {
-    return serverURL + "?" + "text" + input
+    return serverURL + "?" + "text=" + input
 }
 
 function errorHandler(error) {
@@ -19,7 +19,7 @@ function clickHandler() {
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
         .then(json => {
-            var translatedText=json.contents.translated;
+            var translatedText = json.contents.translated;
             outputDiv.innerText=translatedText;
         })
         .catch(errorHandler)
