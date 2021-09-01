df9aec7deec91ad53cf7145efdac8735fb612b30
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-09-01 19:56:51 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input")
-var outputDiv = document.querySelector("#output")
+const btnTranslate = document.querySelector("#btn-translate");
+const txtInput = document.querySelector("#txt-input")
+const outputDiv = document.querySelector("#output")
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
